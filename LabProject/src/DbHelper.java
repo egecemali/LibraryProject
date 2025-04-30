6f9c6f9b66f6fadb00f94716d0e3a9f4bd6cb401
egecemali <cemali_ege@hotmail.com>
2025-04-30 11:06:12 +0300

    Update DbHelper.java

--- LabProject/src/DbHelper.java
+++ LabProject/src/DbHelper.java
@@ -5,6 +5,6 @@ import java.sql.SQLException;
 public class DbHelper {
 
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/Project","postgres","egecem123A.");
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/Project","postgres","password");
     }
 }
